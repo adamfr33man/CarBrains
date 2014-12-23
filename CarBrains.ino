21894952e6156dfdded234a34f0d0d1f3a195605
unknown <cog@ONCALL-VAIO.globalbrain.net>
2014-12-24 01:36:41 +1300

    Added PS2 Controller support

--- CarBrains.ino
+++ CarBrains.ino
@@ -4,10 +4,30 @@
  
  Author: Adam Freeman
  */
-#define PIN_FORWARD 10
-#define PIN_BACKWARD 11
-#define PIN_LEFT 12
-#define PIN_RIGHT 13
+
+/*
+ *  PS2 Controller stuff
+ */
+#include <PS2X_lib.h>  //for v1.6
+
+#define PS2_DAT        2
+#define PS2_CLK        3 
+#define PS2_CMD        4
+#define PS2_ATTN       5
+
+PS2X ps2x; // create PS2 Controller Class
+int  ps2_error = 0;
+byte ps2_type = 0;
+
+boolean ps2_stickmode = false;
+
+/*
+ * Defines for motor control
+ */
+#define PIN_FORWARD 9
+#define PIN_BACKWARD 8
+#define PIN_LEFT 7
+#define PIN_RIGHT 6
 
 #define STOP 0
 #define SLOW 50
@@ -31,7 +51,7 @@ int speed_x = 0; // 0 - 255
 int dir_x = 0; // -1, 0, 1 for left, center, right
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Reset all the Chanels  
   pinMode(PIN_FORWARD, OUTPUT);
@@ -43,15 +63,44 @@ void setup() {
   analogWrite(PIN_BACKWARD, STOP);
   analogWrite(PIN_LEFT, STOP);
   analogWrite(PIN_RIGHT, STOP);
+  
+  // Set PS2 Controller
+  delay(20);
+  
+  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+  ps2_error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_ATTN, PS2_DAT, false, false);
+
+  switch (ps2_error) {
+    case 0:
+      Serial.println("Found Controller, configured successful.");
+      break;
+      
+    case 1:
+      Serial.println("No controller found, check wiring");
+      break;
+      
+    case 2:
+      Serial.println("Controller found but not accepting commands.");
+      break;
+      
+    case 3:
+      Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+      break;
+  }
 }
 
 
 void loop() {
+  // Read the PS2 Controller
+  if(ps2_error != 1) {
+    readController();
+  }
+  
   // Set y direction and speed.
   if(dir_y == DIRECTION_FORWARD) {
     // Forward 
-    analogWrite(PIN_FORWARD, speed_y);
     analogWrite(PIN_BACKWARD, STOP);
+    analogWrite(PIN_FORWARD, speed_y);
   } 
   else if (dir_y == DIRECTION_BACKWARD) {
     // Backward
@@ -72,8 +121,8 @@ void loop() {
   } 
   else if (dir_x == DIRECTION_RIGHT) {
     // Backward
-    analogWrite(PIN_RIGHT, speed_x);
     analogWrite(PIN_LEFT, STOP);    
+    analogWrite(PIN_RIGHT, speed_x);
   } 
   else {
     // Stopped
@@ -82,7 +131,7 @@ void loop() {
   }
 
   // Delay, may be too slow
-  delay(300);  
+  delay(20);  
 }
 
 void serialEvent() {
@@ -90,6 +139,18 @@ void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
 
+    processControlChar(inChar);
+
+    if (inChar == '\n') {
+      serial_part = 0;
+    } 
+    else {
+      serial_part++;
+    }
+  }
+}
+
+void processControlChar(char inChar) {
     switch (inChar) {
     case 'w':
       // Go Forward
@@ -146,12 +207,85 @@ void serialEvent() {
       }
       break;
     }
-    if (inChar == '\n') {
-      serial_part = 0;
-    } 
-    else {
-      serial_part++;
-    }
-  }
 }
 
+void readController() {
+  ps2x.read_gamepad(false, false); 
+  
+  if(ps2x.Button(PSB_PAD_UP)) {
+    processControlChar('w');
+  }
+  
+  if(ps2x.Button(PSB_PAD_DOWN)) {
+    processControlChar('s');
+  }
+
+  if(ps2x.Button(PSB_PAD_LEFT)) {
+    processControlChar('a');
+  }
+
+  if(ps2x.Button(PSB_PAD_RIGHT)) {
+    processControlChar('d');
+  }
+
+  if(ps2x.Button(PSB_L1)) {
+    processControlChar('z');    
+  }
+
+  if(ps2x.Button(PSB_R1)) {
+    processControlChar('q');    
+  }
+
+  if(ps2x.ButtonReleased(PSB_SQUARE)) {
+    processControlChar('f');    
+  }
+
+  if(ps2x.ButtonReleased(PSB_TRIANGLE)) {
+    ps2_stickmode = !ps2_stickmode;
+    Serial.print("Stick mode: ");
+    Serial.println(ps2_stickmode);    
+  }
+  
+  // Read the sticks if enabled.
+  if(ps2_stickmode) {
+    int stick_y = ps2x.Analog(PSS_LY);
+    int stick_x = ps2x.Analog(PSS_RX);
+    
+   if(stick_y == 130) {
+      speed_y = STOP;
+      dir_y = DIRECTION_NEUTRAL;      
+    } else if(stick_y < 130) {
+      speed_y = ((130 - stick_y) * 2);
+      if(speed_y > 255) {
+        speed_y = 255;  
+      } else if(speed_y < SLOW) {
+        speed_y = SLOW;
+      };
+      dir_y = DIRECTION_FORWARD;      
+      Serial.println("Forward");    
+    } else {
+      speed_y = stick_y;
+      dir_y = DIRECTION_BACKWARD;
+      Serial.println("Back");    
+    }
+    
+    if(stick_x == 128) {
+      speed_x = STOP;
+      dir_x = DIRECTION_CENTER;      
+    } else if(stick_x < 130) {
+      speed_x = FULL_SPEED;
+      dir_x = DIRECTION_LEFT;
+      Serial.println("Left");          
+    } else {
+      speed_x = FULL_SPEED;
+      dir_x = DIRECTION_RIGHT; 
+      Serial.println("Right");   
+    }
+    
+    Serial.print("(");
+    Serial.print(stick_x);
+    Serial.print(", ");
+    Serial.print(stick_y);
+    Serial.println(")");
+  }
+}
--- README.md
+++ README.md
@@ -34,3 +34,46 @@ This is the first release point. Assuming you have the car wired up correctly, y
 Note that the y direction is forwards and backawards where as the x direction is left and right. Speed forwards and backwards can be controlled whereas left and right are hard settings (may only be true for this car).
 
 All of this code is assuming that you are controlling the car over a serial connection and is probably only useful for testing.
+
+### v 2.0.0 - PS2 Controller added
+
+Added PS2 Controller, this should allow us to control the car from a PS2 controller without a serial connection (though still wired).
+
+Download and install this library:
+https://github.com/madsci1016/Arduino-PS2X
+
+I also needed to change the pins used to avoid conflicts with the RF module (to come later)
+
+Wiring to the RC car, note this has been changed since the previous release.
+
+| Arduino | Wire     | RC Pin        |
+| ------- | -------- | -------       |
+| 9       | Yellow   | Forward  (11) |
+| 8       | Orange   | Backward (10) |
+| 7       | Green    | Left  (7)     |
+| 6       | Blue     | Right (6)     |
+| GND     | Black    | GND (2)       |
+
+This is the wiring for the PS2 Controller, the wires should match up on most controllers.
+
+| Arduino | PS2 Wire | PS2 Pin      |
+| ------- | -------- | ------------ |
+| 5       | Yellow   | Attention    |
+| 4       | Orange   | Command      |
+| 3       | Blue     | Clock        |
+| 2       | Brown    | Data         |
+| VCC     | Red      | +5V          |
+| GND     | Black    | Ground       |
+
+At this point the Playstation controller will work with the Arrow pad, if you hit a button then that will keep going in the direction you press. Square will stop everything and L1/R1 will decrease and increase speed.
+
+It work better if you hit triangle, this will allow it to use the analog sticks instead. The Left stick will move forwards and backwards. At the moment reverse is only half speed and is not proportional. Forward is proportial however.
+
+The Right stick will control left/right and when centered will center the wheels again. This is not proportial at the moment also.
+
+### Next
+
+Next time I'm planning to add the RF modules.
+
+Info from:
+http://www.billporter.info/2010/06/05/playstation-2-controller-arduino-library-v1-0/
